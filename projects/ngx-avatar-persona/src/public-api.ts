50430aad673fe20a58996ab44b9db1ec04695d5e
Paul Greenberg <greenpau@outlook.com>
2021-12-14 19:53:19 -0500

    add template files

---
+++ projects/ngx-avatar-persona/src/public-api.ts
@@ -0,0 +1,8 @@
+/*
+ * Public API Surface of Auth Portal Service.
+ */
+
+export * from './lib/user-data';
+export * from './lib/auth-portal.service';
+export * from './lib/auth-portal.interceptor';
+export * from './lib/auth-portal-config';
